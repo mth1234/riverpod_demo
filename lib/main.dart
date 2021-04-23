e8f1b64ca56c13b5cbd10f4d239793698c12a32e
mth1234 <mthayk32@gmail.com>
2021-04-24 01:42:47 +0900

    new

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-final counterProvider = StateProvider<int>((ref) => 0);
+final counterProvider =
+    StateNotifierProvider<CounterModel, int>((ref) => CounterModel(0));
+
+class CounterModel extends StateNotifier<int> {
+  CounterModel(int initialNumber) : super(initialNumber);
+
+  void inc() {
+    state++;
+  }
+}
 
 void main() {
   runApp(
@@ -28,7 +37,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final int count = useProvider(counterProvider).state;
+    final int count = useProvider(counterProvider);
 
     return Scaffold(
       body: Center(
@@ -37,7 +46,7 @@ class MyHomePage extends HookWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          context.read(counterProvider).state++;
+          context.read(counterProvider.notifier).inc();
         },
       ),
     );
